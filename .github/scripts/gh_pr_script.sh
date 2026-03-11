4eb4413f4cba8ba30a3ef90b89e6960a5ee04924
Nizam799 <143395973+Nizam799@users.noreply.github.com>
2026-03-11 15:22:01 -0400

    Update gh_pr_script.sh

--- .github/scripts/gh_pr_script.sh
+++ .github/scripts/gh_pr_script.sh
@@ -3,9 +3,9 @@ set -euo pipefail
 git fetch origin main:origin/main
 triggerred_paths='^(deployments/cpsi/global/iam/core_github_team/|\.github/workflows/gh-team-pr\.yml|\.github/workflows/gh-team-apply\.yml|modules/terraform-github-team/terraform-github-team|stacks/iam/github_team/|ansible-aad/.*\.yaml$)'
 
-echo "$MODE"
-[[ "$MODE" == "PR" ]] && DIFF_RANGE="origin/main..HEAD" BASE_REF="origin/main"
-[[ "$MODE" == "MAIN" ]] && DIFF_RANGE="HEAD^1..HEAD" BASE_REF="HEAD~1"
+MODE="MAIN"
+[[ "$MODE" == "PR" ]] && DIFF_RANGE=$(git diff --name-only origin/main..HEAD) BASE_REF="origin/main"
+[[ "$MODE" == "MAIN" ]] && DIFF_RANGE=$(git diff --name-only HEAD^1..HEAD) BASE_REF="HEAD~1"
 echo "$DIFF_RANGE"
 echo "$BASE_REF"
 relevant_files=$(git diff --name-only "$DIFF_RANGE" | grep -E "$TRIGGERED_PATHS" || true)
